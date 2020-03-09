7ba076c355577237d64b1bf9d37824919e2f022b
Алексей Смоляк <alexey.kievskiy@gmail.com>
2020-03-09 12:46:31 +0300

    new functionality with command processing

--- File's for MacBook/bonjour-demo-mac/TrackModel.swift
+++ File's for MacBook/bonjour-demo-mac/TrackModel.swift
@@ -8,51 +8,14 @@
 
 import Foundation
 
-struct TrackList: Codable {
-    var tracksInformation: [TrackInformation]
-    var currentTrack: TrackInformation
+//struct TrackList: Codable {
+//    var tracksInformation: [TrackInformation]
+//    var currentTrack: TrackInformation
+//    
+//    func searchTrack(byTrackName trackName: String) -> TrackInformation? {
+//        return tracksInformation.first { $0.trackName == trackName }
+//    }
+//    
     
-    func searchTrack(byTrackName trackName: String) -> TrackInformation? {
-        return tracksInformation.first { $0.trackName == trackName }
-    }
-    
-    mutating func nextTrack() -> TrackInformation? {
-        
-        if currentTrack.trackName == tracksInformation[tracksInformation.count - 1].trackName {
-            return nil
-        } else {
-            var i = 0
-            for trackInfo in tracksInformation {
-                if currentTrack.trackName == trackInfo.trackName {
-                    currentTrack = tracksInformation[i + 1]
-                    return currentTrack
-                }
-                i = i + 1
-            }
-        }
-        return nil
-    }
-    
-    mutating func prevTrack() -> TrackInformation? {
-        
-        if currentTrack.trackName == tracksInformation[0].trackName {
-            return nil
-        } else {
-            var i = 0
-            for trackInfo in tracksInformation {
-                if currentTrack.trackName == trackInfo.trackName {
-                    currentTrack = tracksInformation[i - 1]
-                    return currentTrack
-                }
-                i = i + 1
-            }
-        }
-        return nil
-    }
-}
+//}
 
-struct TrackInformation: Codable {
-    var trackName: String
-    var albumName: String
-    var imageData: Data
-}
